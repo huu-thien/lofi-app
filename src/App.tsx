359828f444fcf9a1a1708b54606db8bfe430235a
huu-thien <86077619+huu-thien@users.noreply.github.com>
2023-08-06 13:00:22 +0700

    install package + configuration + setup tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 
 
 function App() {
-  return <div className="app">
-    App
-  </div>;
+  return (
+    <div className="app">
+      <h1 className="text-3xl font-bold underline text-gray-primary">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "red-primary": "rgba(180,18,18,0.75)",
+        "black-primary": "rgba(18,18,18,0.75)",
+        "gray-primary": "#858080"
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+      screens: {
+        xs: "480px",
+        sm: "768px",
+        md: "1060px",
+      },
+    },
+  },
+  plugins: [],
+};
